fe1ca794f4c9ee5c86824799f072ee262bc5b194
Tsunami <U16106818@sharjah.ac.ae>
2021-12-15 04:32:17 +0400

    First Commit

--- Testing/App.vb
+++ Testing/App.vb
@@ -1,4 +1,5 @@
 ﻿Public Class App
     Private Sub IrisButton1_Click(sender As Object, e As EventArgs) Handles IrisButton1.Click
+        MessageBox.Show("Email: " + IrisInputBox1.Text + " Password: " + IrisInputBox2.Text)
     End Sub
 End Class
